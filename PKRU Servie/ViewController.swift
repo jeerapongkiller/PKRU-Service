19e61951c5f4d91b1dfac64157de3e27f299143f
PKRU02 <PKRU02@pkru02s-imac.local>
2017-07-28 17:01:12 +0700

    @Success

--- PKRU Servie/ViewController.swift
+++ PKRU Servie/ViewController.swift
@@ -15,12 +15,54 @@ class ViewController: UIViewController {
     var myString = ""
     
     
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var alterLabel: UILabel!
+    @IBAction func loginButton(_ sender: Any) {
+        
+        
+        let strUser = userTextField.text
+        let strPassword = passwordTextField.text
+        
+        
+        
+        if (strUser == "") || (strPassword == ""){
+        
+        //Heve Space
+            alterLabel.text = "Please Fill All Every Blank"
+            
+        }else{
+        //On Space
+            alterLabel.text = ""
+        
+        
+        }
+        
+    }// loginButton
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }//View did load
+    
+    
+    
+    
+    func checkUserAndPass(strUser: String, strPassword: String) -> Void {
+        
+        
+        
+    }//Check User And Password
 
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
